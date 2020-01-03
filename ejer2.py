648cc150c33c6d88c4a4512a511c1f51aac0bf5b
Franco <fnastasi@fi.uba.ar>
2020-01-03 18:26:36 -0300

    se modifico el ejercicio 2 para que tome el nombre del archivo desde filename

--- ejer2.py
+++ ejer2.py
@@ -12,18 +12,20 @@ import planisferioRed
 def ejercicio2(n_fil=2,n_col=2):
     
     # Red de IP
-    fil = 2
-    col = 2
+    #fil = 2
+    #col = 2
     IP_vec = np.array(["192.168.255.0", "192.168.255.1",
                        "192.168.255.2", "192.168.255.3", 
                        ])
 
-    p = planisferioRed.planisferioRed(fil,col,IP_vec)
+    p = planisferioRed.planisferioRed(n_fil,n_col,IP_vec)
 
     # Creación de un satelite
     sat = satelite.satelite("satelite_0003")
     
-    for line in fileinput.input("test.txt", inplace=True): # Recorro cada línea del archivo
+    
+    filename = "test.txt"
+    for line in fileinput.input(filename, inplace=True): # Recorro cada línea del archivo
         try:
             valores =  np.array(line.split()).astype(float) # Parseo los valores
             if valores.size != 0: # Cuando no lees una línea vacía (o una línea lleno de espacios en blanco)
@@ -34,7 +36,7 @@ def ejercicio2(n_fil=2,n_col=2):
             else:
                 line = line[0:-1] # Si estaba vacía, dejo la línea igual (el '\n' lo pone print)
         except:
-            pass # Si no pudo convertir a float, no hace nada
+            line = line[0:-1] # Si no pudo convertir a float, no hace nada
         finally:
             print(line) # imprime la línea en el archivo
     
